4126c617706f427683f0cd296f07a7981eb4ca69
aravindan <aravindan@techaffinity.com>
2020-12-04 13:43:27 +0530

    update object in compound

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -5,11 +5,13 @@ import './CollectionPreview.scss';
 const CollectionPreview = ({title , items}) =>  (
   <div className="collection-preview">
    <h1 className="title">{title.toUpperCase()}</h1>
-   <div className="preview">
-    {items.filter((item,idx) => idx < 4).map(({id, ...iteamprops}) => (
-       <CollectionItem key={id} {...iteamprops} />
-     ))}
-   </div>
+   <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
  </div>
  );
 
